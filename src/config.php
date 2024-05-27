28f3f339bc7ca3e9d8feacef34766be428f5de4c
edwinparrales <edwinparrales82@gmail.com>
2024-05-27 16:45:54 -0500

    Configurando la base de datos

--- src/config.php
+++ src/config.php
@@ -1,9 +1,11 @@
 <?php
-$host = $_ENV('MYSQLHOST');
-$userName = $_ENV('MYSQLUSER');
-$password = $_ENV('MYSQLPASSWORD');
-$dbName = $_ENV('MYSQLDATABASE');
-$port = $_ENV('MYSQLPORT');
+$host = $_ENV['MYSQLHOST'];
+$userName = $_ENV['MYSQLUSER'];
+$password = $_ENV['MYSQLPASSWORD'];
+$dbName = $_ENV['MYSQL_DATABASE'];
+$port = $_ENV['MYSQLPORT'];
+
+
 
 $conn = new mysqli($host, $userName, $password, $dbName, $port);
 if ($conn->connect_error) {
